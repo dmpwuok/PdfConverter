8b8c02d71dac940afe0582a1c508b8befc74cb88
dmpwuok <38777490+dmpwuok@users.noreply.github.com>
2020-08-17 05:04:04 +0530

    Add project files.

---
+++ PdfConverter/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PdfConverter.Models;
+using SelectPdf;
+
+namespace PdfConverter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreatePDF(string html)
+        {
+            html = html.Replace("StrTag", "<").Replace("EndTag", ">");
+
+            HtmlToPdf htmlToPdf = new HtmlToPdf();
+            PdfDocument pdfDocument = htmlToPdf.ConvertHtmlString(html);
+            byte[] pdf = pdfDocument.Save();
+            pdfDocument.Close();
+
+            return File(
+                pdf,
+                "application/pdf",
+                "StudentList.pdf"
+                );
+        }
+    }
+}
